50460ecb636d0607dca82586d86c57430c16bd22
alexgoit <alexandrpuryaev@gmail.com>
2023-07-26 13:19:48 +0300

    refactored

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import Container from '@mui/material/Container';
 
@@ -11,71 +11,63 @@ import getImages from 'services/api';
 
 document.title = 'HW-4 Images';
 
-export default class App extends Component {
-  state = {
-    hits: [],
-    page: 1,
-    searchQuery: '',
-    isLoading: false,
-    showLoadMoreBtn: false,
-  };
+const App = () => {
+  const [hits, setHits] = useState([]);
+  const [page, setPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [showLoadMoreBtn, setShowLoadMoreBtn] = useState(false);
 
-  componentDidUpdate(_, prevState) {
-    const { searchQuery, page } = this.state;
-
-    if (prevState.searchQuery !== searchQuery || page !== prevState.page) {
-      this.fetchImages();
-    }
-  }
-
-  fetchImages = async () => {
-    const { searchQuery, page } = this.state;
-
-    this.setState({ isLoading: true });
-    try {
-      const response = await getImages(searchQuery, page);
-
-      const totalPages = Math.floor(response.totalHits / 12);
-
-      if (response.hits.length === 0) {
-        Notify.warning(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
+  useEffect(() => {
+    const fetchImages = async () => {
+      if (searchQuery === '') {
         return;
       }
 
-      this.setState(({ hits }) => ({
-        hits: [...hits, ...response.hits],
-        page,
-        totalPages,
-      }));
+      setIsLoading(true);
+      try {
+        const response = await getImages(searchQuery, page);
+        const totalPages = Math.ceil(response.totalHits / 12);
 
-      if (page === 1) {
-        Notify.success(`Hooray! We found ${response.totalHits} images.`);
-      } else {
-        setTimeout(() => this.scroll(), 100);
+        if (response.hits.length === 0) {
+          Notify.warning(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+          return;
+        }
+
+        setHits(hits => [...hits, ...response.hits]);
+        setPage(page);
+
+        if (page === 1) {
+          Notify.success(`Hooray! We found ${response.totalHits} images.`);
+        } else {
+          setTimeout(() => scroll(), 100);
+        }
+
+        if (page >= totalPages) {
+          Notify.warning(
+            "We're sorry, but you've reached the end of search results."
+          );
+          setShowLoadMoreBtn(false);
+        } else {
+          setShowLoadMoreBtn(true);
+        }
+      } catch (err) {
+        Notify.failure(`Oops, something went wrong: ${err.message}`);
+      } finally {
+        setIsLoading(false);
       }
+    };
 
-      if (page >= totalPages) {
-        Notify.warning(
-          "We're sorry, but you've reached the end of search results."
-        );
-      }
-    } catch (err) {
-      Notify.failure(`Oops, something went wrong: ${err.message}`);
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  };
+    fetchImages();
+  }, [searchQuery, page]);
 
-  handleSubmit = evt => {
+  const handleSubmit = evt => {
     evt.preventDefault();
 
     const { queryInput } = evt.target.elements;
-
     const searchQuery = queryInput.value.trim();
-    queryInput.value = '';
-    const page = 1;
 
     if (searchQuery === '') {
       Notify.failure(
@@ -84,16 +76,14 @@ export default class App extends Component {
       return;
     }
 
-    this.setState({ searchQuery, page, hits: [] });
+    queryInput.value = '';
+
+    setSearchQuery(searchQuery);
+    setPage(1);
+    setHits([]);
   };
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
-
-  scroll = () => {
+  const scroll = () => {
     const { clientHeight } = document.documentElement;
     window.scrollBy({
       top: clientHeight - 180,
@@ -101,24 +91,24 @@ export default class App extends Component {
     });
   };
 
-  render() {
-    const { hits, isLoading, totalPages, page } = this.state;
-    const isNotEmpty = hits.length !== 0;
-    const isNotEndList = page < totalPages;
+  const isNotEmpty = hits.length !== 0;
 
-    return (
-      <>
-        <Searchbar onSubmit={this.handleSubmit} />
-        <Container maxWidth="lg" sx={{ mt: 2 }}>
-          {isNotEmpty && <ImageGallery hits={hits} />}
-          {isLoading ? (
-            <Loader />
-          ) : (
-            isNotEmpty &&
-            isNotEndList && <Button onLoadMore={this.handleLoadMore} />
-          )}
-        </Container>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Searchbar onSubmit={handleSubmit} />
+      <Container maxWidth="lg" sx={{ pt: 2, pb: 2 }}>
+        {isNotEmpty && <ImageGallery hits={hits} />}
+        {isLoading ? (
+          <Loader />
+        ) : (
+          isNotEmpty &&
+          showLoadMoreBtn && (
+            <Button onLoadMore={() => setPage(page => page + 1)} />
+          )
+        )}
+      </Container>
+    </>
+  );
+};
+
+export default App;
--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const ButtonWrapper = styled.div`
   max-width: calc(100vw - 48px);
   margin-inline: auto;
-  padding-block: 16px;
+  padding-top: 16px;
   display: flex;
   justify-content: center;
   align-items: center;
